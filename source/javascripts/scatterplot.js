9a83f9c9e593c9ac5d6f23601d2a24f0552f412a
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-09 15:03:19 -0400

    clean up scatterplot

--- source/javascripts/scatterplot.js
+++ source/javascripts/scatterplot.js
@@ -2,12 +2,18 @@ $(document).on('ready', function(){
   var w = 500;
   var h = 300;
   var padding = 30;
+  var formatAsPercentage = d3.format(".1%");
 
-  var dataset = [
-    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
-    [600, 150]
-  ];
+
+  var dataset = [];
+  var numDataPoints = 50;
+  var xRange = Math.random() * 1000;
+  var yRange = Math.random() * 1000;
+  for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.round(Math.random() * xRange);
+    var newNumber2 = Math.round(Math.random() * yRange);
+    dataset.push([newNumber1, newNumber2])
+  }
 
   var xScale = d3.scale.linear()
    .domain([0, d3.max(dataset, function(d) { return d[0]; })])
@@ -25,7 +31,8 @@ $(document).on('ready', function(){
   var xAxis = d3.svg.axis()
   .scale(xScale)
   .orient("bottom")
-  .ticks(5);
+  .ticks(5)
+  .tickFormat(formatAsPercentage);
 
   var yAxis = d3.svg.axis()
     .scale(yScale)
@@ -53,22 +60,7 @@ $(document).on('ready', function(){
       return rScale(d[1]);
    });
 
-  svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function(d) {
-      return d[0] + "," + d[1];
-   })
-   .attr("x", function(d) {
-      return xScale(d[0]);
-   })
-   .attr("y", function(d) {
-      return yScale(d[1]);
-   })
-   .attr("font-family", "sans-serif")
-   .attr("font-size", "11px")
-   .attr("fill", "red");
+
 
   svg.append("g")
     .attr("class", "axis")
