9d4d4d774cb40b5665b2c2bb95d33a4621cc3006
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-09 11:55:04 -0400

    add paragraphs

---
+++ source/javascripts/d3.js
@@ -0,0 +1,15 @@
+
+$(document).on('ready', function(){
+
+      var dataset = [ 5, 10, 15, 20, 25 ];
+
+      d3.select("body").selectAll("p")
+        .data(dataset)
+        .enter()
+        .append("p")
+        .text("New paragraph!");
+
+
+
+
+})
