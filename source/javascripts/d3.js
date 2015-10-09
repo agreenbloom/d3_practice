cd0200d5dee198060890822725f1423fd4df2eb8
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-09 13:40:34 -0400

    completed first bar chart

--- source/javascripts/d3.js
+++ source/javascripts/d3.js
@@ -1,40 +1,50 @@
-
 $(document).on('ready', function(){
+  var w = 500;
+  var h = 100;
+  var barPadding = 1;
 
-      var dataset = [  ];
+  var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+          11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-      for (var i = 0; i < 25; i ++) {
-        var newNumber = Math.random() * 30;
-        dataset.push(newNumber);
-      }
-      console.log(dataset)
+  var svg = d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
-      d3.select("body").selectAll("div")
-        .data(dataset)
-        .enter()
-        .append("div")
-        .attr("class", "bar")
-        .style("height", function(d){
-          var barHeight = d * 5;
-          return barHeight + "px";
-        });
+  svg.selectAll("rect")
+     .data(dataset)
+     .enter()
+     .append("rect")
+     .attr("x", function(d, i) {
+        return i * (w / dataset.length);
+     })
+     .attr("y", function(d){
+      return h - (d * 4);
+     })
+     .attr("width", w / dataset.length - barPadding)
+     .attr("height", function(d){
+      return d * 4;
+     })
+     .attr("fill", function(d){
+       return "rgb(0, 0, " + (d * 10) + ")";
+     });
 
-      // d3.select("body").selectAll("p")
-      //   .data(dataset)
-      //   .enter()
-      //   .append("p")
-      //   .text(function(d) {
-      //     return "I can count up to " + d;
-      //   })
-      // .style("color", function(d){
-      //   if (d > 15 ) {
-      //     return "red";
-      //   } else {
-      //     return "black";
-      //   }
-      // });
-
-
-
+  svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d){
+      return d;
+    })
+    .attr("x", function(d, i) {
+        return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+     })
+     .attr("y", function(d){
+      return h - (d * 4) + 14;
+     })
+     .attr("font-family", "sans-serif")
+     .attr("font-size", "11px")
+     .attr("fill", "white")
+     .attr("text-anchor", "middle")
 
 })
