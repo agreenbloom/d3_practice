0bd5ac8b3a38e7386d72319eac4a21fa5cbfe2f9
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-09 12:19:12 -0400

    small bar chart

--- source/javascripts/d3.js
+++ source/javascripts/d3.js
@@ -3,14 +3,29 @@ $(document).on('ready', function(){
 
       var dataset = [ 5, 10, 15, 20, 25 ];
 
-      d3.select("body").selectAll("p")
+      d3.select("body").selectAll("div")
         .data(dataset)
         .enter()
-        .append("p")
-        .text(function(d) {
-          return "I can count up to" + d;
+        .append("div")
+        .attr("class", "bar")
+        .style("height", function(d){
+          return d + "px";
         });
 
+      // d3.select("body").selectAll("p")
+      //   .data(dataset)
+      //   .enter()
+      //   .append("p")
+      //   .text(function(d) {
+      //     return "I can count up to " + d;
+      //   })
+      // .style("color", function(d){
+      //   if (d > 15 ) {
+      //     return "red";
+      //   } else {
+      //     return "black";
+      //   }
+      // });
 
 
 
