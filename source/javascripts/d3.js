c36b9bbc7dd370af21aab5f5b1770b15da2f1a3e
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-09 12:29:06 -0400

    add larger dataset

--- source/javascripts/d3.js
+++ source/javascripts/d3.js
@@ -1,7 +1,9 @@
 
 $(document).on('ready', function(){
 
-      var dataset = [ 5, 10, 15, 20, 25 ];
+      var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+                24, 18, 25, 9, 3 ];;
 
       d3.select("body").selectAll("div")
         .data(dataset)
@@ -9,7 +11,8 @@ $(document).on('ready', function(){
         .append("div")
         .attr("class", "bar")
         .style("height", function(d){
-          return d + "px";
+          var barHeight = d * 5;
+          return barHeight + "px";
         });
 
       // d3.select("body").selectAll("p")
