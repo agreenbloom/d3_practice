a1dd063e894732363e7dc4df85cc2ae779f828c1
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-10 22:38:12 -0400

    clip path on scatter plot

--- source/javascripts/scatterplot.js
+++ source/javascripts/scatterplot.js
@@ -4,6 +4,7 @@ $(document).on('ready', function(){
   var h = 300;
   var padding = 30;
 
+
   var dataset = [];
   var numDataPoints = 50;
   var maxRange = Math.random() * 1000;
@@ -13,54 +14,68 @@ $(document).on('ready', function(){
     dataset.push([newNumber1, newNumber2]);
   }
 
+
   var xScale = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-  .range([padding, w - padding * 2]);
+    .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+    .range([padding, w - padding * 2]);
 
   var yScale = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-  .range([h - padding, padding]);
+    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+    .range([h - padding, padding]);
+
 
   var xAxis = d3.svg.axis()
-  .scale(xScale)
-  .orient("bottom")
-  .ticks(5);
+    .scale(xScale)
+    .orient("bottom")
+    .ticks(5);
 
   var yAxis = d3.svg.axis()
-  .scale(yScale)
-  .orient("left")
-  .ticks(5);
+    .scale(yScale)
+    .orient("left")
+    .ticks(5);
 
   var svg = d3.select("body")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-  svg.selectAll("circle")
-  .data(dataset)
-  .enter()
-  .append("circle")
-  .attr("cx", function(d) {
-    return xScale(d[0]);
-  })
-  .attr("cy", function(d) {
-    return yScale(d[1]);
-  })
-  .attr("r", 2);
+  svg.append("clipPath")
+    .attr("id", "chart-area")
+    .append("rect")
+    .attr("x", padding)
+    .attr("y", padding)
+    .attr("width", w - padding * 3)
+    .attr("height", h - padding * 2);
 
   svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + (h - padding) + ")")
-  .call(xAxis);
+   .attr("id", "circles")
+   .attr("clip-path", "url(#chart-area)")
+   .selectAll("circle")
+   .data(dataset)
+   .enter()
+   .append("circle")
+   .attr("cx", function(d) {
+      return xScale(d[0]);
+   })
+   .attr("cy", function(d) {
+      return yScale(d[1]);
+   })
+   .attr("r", 2);
+
 
   svg.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
 
   d3.select("p")
-  .on("click", function() {
-
+    .on("click", function() {
       var numValues = dataset.length;
       var maxRange = Math.random() * 1000;
       dataset = [];
@@ -70,17 +85,18 @@ $(document).on('ready', function(){
         dataset.push([newNumber1, newNumber2]);
       }
 
-      xScale.domain([0, d3.max(dataset, function(d) { return d[0]; })]);
-      yScale.domain([0, d3.max(dataset, function(d) { return d[1]; })]);
+  xScale.domain([0, d3.max(dataset, function(d) { return d[0]; })]);
+  yScale.domain([0, d3.max(dataset, function(d) { return d[1]; })]);
 
-      svg.selectAll("circle")
-        .data(dataset)
-        .transition()
-        .duration(1000)
-        .each("start", function() {
-         d3.select(this)
-         .attr("fill", "magenta")
-         .attr("r", 7);
+    //Update all circles
+    svg.selectAll("circle")
+      .data(dataset)
+      .transition()
+      .duration(1000)
+      .each("start", function() {
+       d3.select(this)
+       .attr("fill", "magenta")
+       .attr("r", 7);
      })
       .attr("cx", function(d) {
         return xScale(d[0]);
@@ -93,16 +109,21 @@ $(document).on('ready', function(){
       .attr("fill", "black")
       .attr("r", 2);
 
-      svg.select(".x.axis")
-        .transition()
-        .duration(1000)
-        .call(xAxis);
+    //Update X axis
+    svg.select(".x.axis")
+      .transition()
+      .duration(1000)
+      .call(xAxis);
 
-      svg.select(".y.axis")
-        .transition()
-        .duration(1000)
-        .call(yAxis);
+    //Update Y axis
+    svg.select(".y.axis")
+      .transition()
+      .duration(1000)
+      .call(yAxis);
 
-        });
+  });
+
+
+
 
 });
