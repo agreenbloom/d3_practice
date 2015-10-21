041514a4c81df7137ced8ea6215032e9f0cb81b4
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-21 16:10:52 -0400

    rotating globe

--- source/javascripts/globe.js
+++ source/javascripts/globe.js
@@ -3,21 +3,35 @@ $(document).on('ready', function(){
   var width = 960;
   var height = 960;
 
+var radius = height / 2-5,
+  scale = radius,
+  velocity = .02;
+
   var projection  = d3.geo.orthographic()
-    .scale(475)
+    .scale(scale)
     .translate([width / 2, height / 2])
     .clipAngle(90)
     .precision(.1);
 
+  var canvas = d3.select("body").append("canvas")
+    .attr("width", width)
+    .attr("height", height);
+
+  var context = canvas.node().getContext("2d");
+
   var path = d3.geo.path()
-    .projection(projection);
+    .projection(projection)
+    .context(context);
 
   var graticule = d3.geo.graticule();
 
 
   var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", height)
+    .attr("height", width);
+
+ var globe = {type: "Sphere"};
+
 
   var features = svg.append("g")
     .attr("class","features");
@@ -39,11 +53,28 @@ $(document).on('ready', function(){
       .append("path")
       .attr("d",path)
       .attr("class", "countries")
-      .on("click",clicked)
+      // .on("click",clicked)
       .style("fill", function(d) {
-      return newFeatureColor(geoPath.area(d));
+      return newFeatureColor(path.area(d));
     })
+
+    d3.timer(function(elapsed) {
+      context.clearRect(0, 0, width, height);
+
+      projection.rotate([velocity * elapsed, 0]);
+      context.beginPath();
+      path(geodata);
+      context.fill();
+
+      context.beginPath();
+      context.arc(width / 2, height / 2, radius, 0, 2 * Math.PI, true);
+      context.lineWidth = 2.5;
+      context.stroke();
+      });
+
   });
+
+  d3.select(self.frameElement).style("height", height + "px");
 
   d3.select("svg").append("path")
     .datum(graticule)
@@ -53,13 +84,17 @@ $(document).on('ready', function(){
     .style("stroke", "grey")
     .style("stroke-width", "1px");
 
-    d3.select("svg").append("path")
-      .datum(graticule.outline)
-      .attr("class", "graticule outline")
-      .attr("d", path)
-      .style("fill", "none")
-      .style("stroke", "black")
-      .style("stroke-width", "1px");
+
+  d3.select("svg").append("path")
+    .datum(graticule.outline)
+    .attr("class", "graticule outline")
+    .attr("d", path)
+    .style("fill", "none")
+    .style("stroke", "black")
+    .style("stroke-width", "1px");
+
+
+
 
 
 });
