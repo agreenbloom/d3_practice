aea804f03ce21e4a63e3456420cc61605d72dcfb
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-25 19:10:58 -0400

    render map of toronto

--- source/javascripts/toronto.js
+++ source/javascripts/toronto.js
@@ -1,10 +1,14 @@
 $(document).on('ready', function(){
 
-  var width = 462,
-      height = 600;
+  var width = 500,
+      height = 500;
 
   var projection = d3.geo.conicEqualArea()
-
+      .scale(height * 100)
+      .center([ -79.20831973072589, 43.739257904235977 ])
+      // .parallels([41.9714969544088,83.1480859363606])
+      // .rotate([96.64103465647952])
+      .translate([height / 2, width / 2]);
 
   var path = d3.geo.path()
       .projection(projection);
@@ -13,44 +17,32 @@ $(document).on('ready', function(){
       .attr("width", width)
       .attr("height", height);
 
-  var zoom = d3.behavior.zoom()
-      .scaleExtent([1, Infinity])
-      .on("zoom",zoomed);
-
-  svg.call(zoom);
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height);
 
   var features = svg.append("g")
       .attr("class","features");
 
-  var zoom = d3.behavior.zoom()
-      .scaleExtent([1, Infinity])
-      .on("zoom",zoomed);
+  // var zoom = d3.behavior.zoom()
+  //     .scaleExtent([1, Infinity])
+  //     .on("zoom",zoomed);
 
-  svg.call(zoom);
+  // svg.call(zoom);
 
-  d3.json("ontario.json",function(error,geodata) {
+  d3.json("toronto.geojson", function(error,geodata) {
     if (error) return console.log(error);
 
     features.selectAll("path")
       .data(geodata.features)
       .enter()
       .append("path")
-      .attr("d",path)
-      .on("click",clicked);
+      .attr("d",path);
+      // .on("click",clicked);
 
   });
 
 
-  function clicked(d,i) {
 
-  }
-
-  function zoomed() {
-    features.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")")
-        .selectAll("path").style("stroke-width", 1 / zoom.scale() + "px" );
-  }
+  // function zoomed() {
+  //   features.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")")
+  //       .selectAll("path").style("stroke-width", 1 / zoom.scale() + "px" );
+  // }
 });
