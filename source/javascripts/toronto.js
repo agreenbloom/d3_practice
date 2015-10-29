ce157f23b334e0d04cca641ebe3a778df959ceb3
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-29 14:12:56 -0400

    refactor to add queue - set up to add data to map

--- source/javascripts/toronto.js
+++ source/javascripts/toronto.js
@@ -6,8 +6,6 @@ $(document).on('ready', function(){
   var projection = d3.geo.conicEqualArea()
       .scale(145500)
       .center([-79.45318304409768,43.74080098090873])
-      // .parallels([41.9714969544088,83.1480859363606])
-      // .rotate([96.64103465647952])
       .translate([height / 2, width / 2]);
 
   var path = d3.geo.path()
@@ -21,13 +19,14 @@ $(document).on('ready', function(){
   var features = svg.append("g")
       .attr("class","features");
 
-  // var zoom = d3.behavior.zoom()
-  //     .scaleExtent([1, Infinity])
-  //     .on("zoom",zoomed);
 
-  // svg.call(zoom);
+  queue()
+    .defer(d3.json, "toronto.geojson")
+    .defer(d3.csv, "uber.csv")
+    .await(ready);
 
-  d3.json("toronto.geojson", function(error,geodata) {
+
+  function ready(error, geodata) {
     if (error) return console.log(error);
 
     features.selectAll("path")
@@ -35,14 +34,8 @@ $(document).on('ready', function(){
       .enter()
       .append("path")
       .attr("d",path);
-      // .on("click",clicked);
 
-  });
-
+  };
 
 
-  // function zoomed() {
-  //   features.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")")
-  //       .selectAll("path").style("stroke-width", 1 / zoom.scale() + "px" );
-  // }
 });
