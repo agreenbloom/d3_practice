e618152352abcc8d66809ff70623f17a71ba50be
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-31 12:54:30 -0400

    change toronto to topojson

--- source/javascripts/toronto.js
+++ source/javascripts/toronto.js
@@ -1,10 +1,16 @@
 $(document).on('ready', function(){
 
-  var width = 940,
-      height = 860;
+  var width = 1230,
+      height = 1160;
 
-  var projection = d3.geo.conicEqualArea()
-      .scale(145500)
+  var rateById = d3.map();
+
+  var quantize = d3.scale.quantize()
+    .domain([0, .15])
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
+  var projection = d3.geo.mercator()
+      .scale(135500)
       .center([-79.45318304409768,43.74080098090873])
       .translate([height / 2, width / 2]);
 
@@ -20,22 +26,28 @@ $(document).on('ready', function(){
       .attr("class","features");
 
 
+
   queue()
     .defer(d3.json, "toronto.geojson")
     .defer(d3.csv, "uber.csv")
     .await(ready);
 
 
-  function ready(error, geodata) {
+  function ready(error, geodata, uber) {
     if (error) return console.log(error);
 
-    features.selectAll("path")
+
+      uber.forEach(function(d) {
+        console.log(d.rate);
+        console.log(d.HOOD);
+        console.log(d.id);
+      });
+
+    svg.selectAll("path")
       .data(geodata.features)
       .enter()
       .append("path")
       .attr("d",path);
-
   };
-
 
 });
